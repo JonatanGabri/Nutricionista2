1fac68058d0428fb62a546350df0de170f372b6e
RubenOPG5 <rosquilla2022@gmail.com>
2023-11-02 15:42:43 -0300

    Revert "www"
    This reverts commit a18277e944da4a0e2cd012d46cdb23c23cbe9778.

--- Nutricionista_2/src/Vistas/ComidaView.java
+++ Nutricionista_2/src/Vistas/ComidaView.java
@@ -15,10 +15,8 @@ import javax.swing.JOptionPane;
  */
 public class ComidaView extends javax.swing.JInternalFrame {
 
-    ComidaData comidaData = new ComidaData();
+    ComidaData comidaData= new ComidaData();
     Comida comidaActual = null;
-    
-
     public ComidaView() {
         initComponents();
     }
@@ -44,8 +42,6 @@ public class ComidaView extends javax.swing.JInternalFrame {
         jbSalir = new javax.swing.JButton();
         jbNuevo = new javax.swing.JButton();
         jButton1 = new javax.swing.JButton();
-        JbBuscar = new javax.swing.JButton();
-        jBmodificar = new javax.swing.JButton();
 
         setTitle("COMIDA");
 
@@ -97,7 +93,7 @@ public class ComidaView extends javax.swing.JInternalFrame {
         });
 
         jbNuevo.setFont(new java.awt.Font("Tahoma", 0, 14)); // NOI18N
-        jbNuevo.setText("Limpiar");
+        jbNuevo.setText("Nuevo");
         jbNuevo.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 jbNuevoActionPerformed(evt);
@@ -112,90 +108,64 @@ public class ComidaView extends javax.swing.JInternalFrame {
             }
         });
 
-        JbBuscar.setText("Buscar");
-        JbBuscar.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                JbBuscarActionPerformed(evt);
-            }
-        });
-
-        jBmodificar.setText("Modificar");
-        jBmodificar.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jBmodificarActionPerformed(evt);
-            }
-        });
-
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
         jPanel1.setLayout(jPanel1Layout);
         jPanel1Layout.setHorizontalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(jPanel1Layout.createSequentialGroup()
-                .addGap(154, 154, 154)
-                .addComponent(jLabel1)
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
-            .addGroup(jPanel1Layout.createSequentialGroup()
-                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
-                        .addGap(8, 8, 8)
-                        .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
-                            .addGroup(jPanel1Layout.createSequentialGroup()
-                                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                                    .addComponent(jLabel2)
-                                    .addComponent(jLabel3))
-                                .addGap(18, 18, 18)
-                                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                                    .addComponent(jtDetalle)
-                                    .addComponent(jtNombre)))
-                            .addGroup(jPanel1Layout.createSequentialGroup()
-                                .addComponent(jLabel4)
-                                .addGap(18, 18, 18)
-                                .addComponent(jtcantCalorias)))
-                        .addGap(18, 18, 18)
-                        .addComponent(JbBuscar))
+                .addGap(29, 29, 29)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                    .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addComponent(jLabel4)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 25, Short.MAX_VALUE)
+                        .addComponent(jtcantCalorias, javax.swing.GroupLayout.PREFERRED_SIZE, 153, javax.swing.GroupLayout.PREFERRED_SIZE))
                     .addGroup(jPanel1Layout.createSequentialGroup()
                         .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                            .addGroup(jPanel1Layout.createSequentialGroup()
-                                .addGap(12, 12, 12)
-                                .addComponent(jbNuevo)
-                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                                .addComponent(jbGuardar)
-                                .addGap(12, 12, 12)
-                                .addComponent(jBmodificar)
-                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                                .addComponent(jButton1))
-                            .addGroup(jPanel1Layout.createSequentialGroup()
-                                .addGap(148, 148, 148)
-                                .addComponent(jbSalir, javax.swing.GroupLayout.PREFERRED_SIZE, 77, javax.swing.GroupLayout.PREFERRED_SIZE)))
-                        .addGap(0, 12, Short.MAX_VALUE)))
-                .addContainerGap())
+                            .addComponent(jLabel3)
+                            .addComponent(jLabel2))
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                        .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING, false)
+                            .addComponent(jtDetalle)
+                            .addComponent(jtNombre, javax.swing.GroupLayout.DEFAULT_SIZE, 211, Short.MAX_VALUE))))
+                .addGap(46, 46, 46))
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addComponent(jLabel1)
+                .addGap(162, 162, 162))
+            .addGroup(jPanel1Layout.createSequentialGroup()
+                .addGap(20, 20, 20)
+                .addComponent(jbNuevo)
+                .addGap(18, 18, 18)
+                .addComponent(jButton1)
+                .addGap(18, 18, 18)
+                .addComponent(jbGuardar)
+                .addGap(18, 18, 18)
+                .addComponent(jbSalir)
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
         jPanel1Layout.setVerticalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(jPanel1Layout.createSequentialGroup()
                 .addComponent(jLabel1)
-                .addGap(18, 18, 18)
+                .addGap(20, 20, 20)
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(jtNombre, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addComponent(jLabel2)
-                    .addComponent(JbBuscar))
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 26, Short.MAX_VALUE)
+                    .addComponent(jtNombre, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 41, Short.MAX_VALUE)
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(jLabel3)
                     .addComponent(jtDetalle, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addGap(28, 28, 28)
+                .addGap(40, 40, 40)
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(jLabel4, javax.swing.GroupLayout.PREFERRED_SIZE, 20, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addComponent(jtcantCalorias, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addGap(34, 34, 34)
+                .addGap(45, 45, 45)
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(jbGuardar)
+                    .addComponent(jbSalir)
                     .addComponent(jbNuevo)
-                    .addComponent(jBmodificar)
                     .addComponent(jButton1))
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                .addComponent(jbSalir)
-                .addGap(17, 17, 17))
+                .addGap(29, 29, 29))
         );
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
@@ -213,29 +183,28 @@ public class ComidaView extends javax.swing.JInternalFrame {
     }// </editor-fold>//GEN-END:initComponents
 
     private void jbGuardarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jbGuardarActionPerformed
-        try {
-            String nombre = jtNombre.getText();
-            String detalle = jtDetalle.getText();
-            Integer cantCalorias = Integer.parseInt(jtcantCalorias.getText());
-            if (nombre.isEmpty() || detalle.isEmpty() || cantCalorias.toString() == "") {
-                JOptionPane.showMessageDialog(this, "No puede haber campos vacios");
-                return;//sale del metodo
-            }
-            if (comidaActual == null) {
-                comidaActual = new Comida(nombre, detalle, cantCalorias);
-                comidaData.agregarComida(comidaActual);
-             limpiarCampos();   
-
-            } else {
-                comidaActual.setNombre(nombre);
-                comidaActual.setDetalle(detalle);
-                comidaActual.setCantCalorias(cantCalorias);
-                comidaData.modificarComida(comidaActual);
-            }
-        } catch (NumberFormatException ex) {
-            JOptionPane.showMessageDialog(this, "Debe escrbir enteros");
+       try{
+        String nombre = jtNombre.getText();
+        String detalle = jtDetalle.getText();
+        Integer cantCalorias = Integer.parseInt(jtcantCalorias.getText());
+        if(nombre.isEmpty()||detalle.isEmpty()||cantCalorias.toString()==""){
+         JOptionPane.showMessageDialog(this,"No puede haber campos vacios");
+       return;//sale del metodo
         }
-
+        if(comidaActual==null){
+            comidaActual= new Comida(nombre,detalle,cantCalorias);
+            comidaData.agregarComida(comidaActual);
+            
+        }else{
+        comidaActual.setNombre(nombre);
+        comidaActual.setDetalle(detalle);
+        comidaActual.setCantCalorias(cantCalorias);
+        comidaData.modificarComida(comidaActual);
+        }
+       }   catch(NumberFormatException ex){
+        JOptionPane.showMessageDialog(this, "Debe escrbir enteros");
+        }
+          
     }//GEN-LAST:event_jbGuardarActionPerformed
 
     private void jtNombreActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jtNombreActionPerformed
@@ -247,82 +216,27 @@ public class ComidaView extends javax.swing.JInternalFrame {
     }//GEN-LAST:event_jtcantCaloriasActionPerformed
 
     private void jbNuevoActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jbNuevoActionPerformed
-        limpiarCampos();
-        comidaActual = null;
-
+       limpiarCampos();
+       comidaActual= null;
+        
     }//GEN-LAST:event_jbNuevoActionPerformed
 
     private void jbSalirActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jbSalirActionPerformed
-        dispose();//solo sale la ventana Comida
-        //System.exit(0); // se cierra por completo el programa
+      dispose();//solo sale la ventana Comida
+     //System.exit(0); // se cierra por completo el programa
     }//GEN-LAST:event_jbSalirActionPerformed
 
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
-
-        if (comidaActual != null) {
-            comidaData.eliminarComida(comidaActual.getIdComida());
-            limpiarCampos();
-        } else {
-            JOptionPane.showMessageDialog(this, "No hay comida selecionado");
+        
+        if(comidaActual!=null){
+        comidaData.eliminarComida(comidaActual.getIdComida());
+        }else{
+        JOptionPane.showMessageDialog(this, "No hay comida selecionado");
         }
     }//GEN-LAST:event_jButton1ActionPerformed
 
-    private void JbBuscarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_JbBuscarActionPerformed
-        try {
-        String nombre = jtNombre.getText();
-
-        if (nombre.isEmpty()) {
-            JOptionPane.showMessageDialog(this, "Debes ingresar un nombre para buscar.");
-        } else {
-            // Realiza la búsqueda de la comida por nombre
-            Comida comidaEncontrada = comidaData.buscarComidaPorNombre(nombre);
-
-            if (comidaEncontrada != null) {
-                // Llena los campos con los detalles y las calorías de la comida encontrada
-                jtDetalle.setText(comidaEncontrada.getDetalle());
-                jtcantCalorias.setText(String.valueOf(comidaEncontrada.getCantCalorias()));
-                comidaActual = comidaEncontrada; // Asegúrate de que se llena comidaActual
-                System.out.println("Comida actual: " + comidaActual.getNombre());
-            } else {
-                // Manejar el caso en que no se encontró la comida
-            }
-        }
-    } catch (NumberFormatException ex) {
-        JOptionPane.showMessageDialog(this, "Debe ingresar un nombre válido.");
-    }
-    }//GEN-LAST:event_JbBuscarActionPerformed
-
-    private void jBmodificarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jBmodificarActionPerformed
-        if (comidaActual != null) {
-            // Se encontró una comida previamente
-            String nuevoNombre = jtNombre.getText();
-            String nuevoDetalle = jtDetalle.getText();
-            Integer nuevaCantCalorias = Integer.parseInt(jtcantCalorias.getText());
-
-            // Verificar si hay campos vacíos
-            if (nuevoNombre.isEmpty() || nuevoDetalle.isEmpty() || jtcantCalorias.getText().isEmpty()) {
-                JOptionPane.showMessageDialog(this, "No puede haber campos vacíos");
-                return; // Sal del método
-            }
-
-            // Actualizar los datos de la comida
-            comidaActual.setNombre(nuevoNombre);
-            comidaActual.setDetalle(nuevoDetalle);
-            comidaActual.setCantCalorias(nuevaCantCalorias);
-
-            // Llamar al método modificarComida para actualizar la comida en la base de datos
-            comidaData.modificarComida(comidaActual);
-            limpiarCampos();
-        } else {
-            // No se encontró una comida para modificar
-            JOptionPane.showMessageDialog(this, "Primero busca una comida para modificar.");
-        }
-    }//GEN-LAST:event_jBmodificarActionPerformed
-
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JButton JbBuscar;
-    private javax.swing.JButton jBmodificar;
     private javax.swing.JButton jButton1;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
@@ -336,10 +250,10 @@ public class ComidaView extends javax.swing.JInternalFrame {
     private javax.swing.JTextField jtNombre;
     private javax.swing.JTextField jtcantCalorias;
     // End of variables declaration//GEN-END:variables
-private void limpiarCampos() {
-        jtNombre.setText("");
-        jtDetalle.setText("");
-        jtcantCalorias.setText("");
+private void limpiarCampos(){
+jtNombre.setText("");
+jtDetalle.setText("");
+jtcantCalorias.setText("");
 
-    }
+}
 }
--- Nutricionista_2/src/Vistas/PacienteVista.java
+++ Nutricionista_2/src/Vistas/PacienteVista.java
@@ -104,7 +104,7 @@ public class PacienteVista extends javax.swing.JFrame {
             }
         });
 
-        jbNuevo.setText("Limpiar");
+        jbNuevo.setText("Nuevo");
         jbNuevo.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 jbNuevoActionPerformed(evt);
@@ -187,11 +187,11 @@ public class PacienteVista extends javax.swing.JFrame {
                             .addGroup(jPanel1Layout.createSequentialGroup()
                                 .addComponent(jbNuevo)
                                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                                .addComponent(jbGuardar)
+                                .addComponent(jbEliminar)
                                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                                 .addComponent(jButton1)
-                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                                .addComponent(jbEliminar)
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                                .addComponent(jbGuardar)
                                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                                 .addComponent(jbSalir))))
                     .addGroup(jPanel1Layout.createSequentialGroup()
@@ -229,20 +229,14 @@ public class PacienteVista extends javax.swing.JFrame {
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addComponent(jLabel7)
                     .addComponent(jdcFecha, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addGroup(jPanel1Layout.createSequentialGroup()
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                        .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                            .addComponent(jbNuevo)
-                            .addComponent(jbSalir))
-                        .addGap(30, 30, 30))
-                    .addGroup(jPanel1Layout.createSequentialGroup()
-                        .addGap(50, 50, 50)
-                        .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                            .addComponent(jButton1)
-                            .addComponent(jbEliminar)
-                            .addComponent(jbGuardar))
-                        .addContainerGap(29, Short.MAX_VALUE))))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 43, Short.MAX_VALUE)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jbNuevo)
+                    .addComponent(jbEliminar)
+                    .addComponent(jButton1)
+                    .addComponent(jbGuardar)
+                    .addComponent(jbSalir))
+                .addGap(30, 30, 30))
         );
 
         jDesktopPane1.setLayer(jPanel1, javax.swing.JLayeredPane.DEFAULT_LAYER);
@@ -337,7 +331,6 @@ public class PacienteVista extends javax.swing.JFrame {
         if (pacienteActual != null) {
             pacienteData.eliminarPaciente(pacienteActual.getIdPaciente());
             pacienteActual = null;
-            limpiarCampos();
             limpiarCampos();
         } else {
             JOptionPane.showMessageDialog(this, "No hay un paciente selecionado");
